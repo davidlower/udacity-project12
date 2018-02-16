0a94ff64002d883abb7f16407ea62adceb4d4ef7
David Lower <davidlower4@gmail.com>
2018-02-16 09:58:48 +0000

    h

--- scripts.js
+++ scripts.js
@@ -4,47 +4,53 @@ $(document).ready(function() {
 
    $(".image-1").on("click", function() {
       $(".overlay-1").show();
-      $(".close").show();
+      $(".gallery").hide();
    });
 
    $(".image-2").on("click", function() {
       $(".overlay-2").show();
-      $(".close").show();
+      $(".gallery").hide();
    });
 
    $(".image-3").on("click", function() {
       $(".overlay-3").show();
-      $(".close").show();
+      $(".gallery").hide();
    });
 
    $(".image-4").on("click", function() {
       $(".overlay-4").show();
-      $(".close").show();
+      $(".gallery").hide();
    });
 
    $(".image-5").on("click", function() {
       $(".overlay-5").show();
-      $(".close").show();
+      $(".gallery").hide();
    });
 
    $(".image-6").on("click", function() {
       $(".overlay-6").show();
-      $(".close").show();
+      $(".gallery").hide();
    });
 
 
    // CLOSE THE OVERLAY
 
-   $(".close").on("click", function() {
-      $(".large-overlay").hide();
-      $(this).hide();
-   });
-
    $(".large-overlay").on("click", function() {
       $(this).hide();
       $(".close").hide();
+      $(".gallery").show();
    });
 
-
-
 });
+
+// OTHER CODE SUGGESTED BY SOMEONE ELSE - CAN'T QUITE GET MY HEAD AROUND IT
+
+// $(document).ready(function() {
+//   function openOverlay() {
+//     const imageNumber = $(this).children('p').attr('class').slice(-1);
+//     const overlayClass = '.overlay-' + imageNumber;
+//     $(overlayClass).show();
+//     $('.close').show();
+//   }
+
+//   $(".img-overlay").on("click", openOverlay);
